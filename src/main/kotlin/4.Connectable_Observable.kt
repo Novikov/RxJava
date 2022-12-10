6742c208fae45e8b3e29a5359293b12a45285de2
Igor Novikov <igor.novikov@umbrellait.com>
2022-12-10 18:43:42 +0300

    connectable observable

--- src/main/kotlin/4.Connectable_Observable.kt
+++ src/main/kotlin/4.Connectable_Observable.kt
@@ -2,8 +2,11 @@ import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
 fun main() {
-    //    connectableObservableExample()
-    connectableObservableExample2()
+    //connectableObservableExample()
+    //connectableObservableExample2()
+    //connectableObservableExample3()
+    //connectableObservableExample4()
+    connectableObservableExample5()
 }
 
 /** Connectable Observable - разновидность hot emitter*/
@@ -25,6 +28,8 @@ fun connectableObservableExample() {
     Thread.sleep(5000)
 }
 
+/** Life cycle
+ * после выполнения dispose для первой подписки observable продолжает жить и вторая подписка будет получать следующие значения*/
 fun connectableObservableExample2() {
     val observable = Observable.interval(1, TimeUnit.SECONDS)
         .doOnDispose { println("Observable dispose") }
@@ -40,7 +45,6 @@ fun connectableObservableExample2() {
         println("sub 1 complete")
     })
 
-    //после выполнения dispose для первой подписки observable продолжает жить и вторая подписка будет получать следующие значения
     val sub2 = observable.subscribe({
         println("sub 2 received $it")
         Thread.sleep(500)
@@ -55,6 +59,135 @@ fun connectableObservableExample2() {
     Thread.sleep(3000)
 
     sub1.dispose()
+
+    Thread.sleep(10000)
+}
+
+/** Share - это обертка над publish().refcount()
+ * refcount() - возращает Observable(), а не ConnecntableObservable().
+ * Этот Observable будет жить до тех пор пока есть хоть одна подписка на него (т.е делаем его cold)
+ * Если вызвать dispose на всех подписках, то эмиссия прекратится. При повторной подписке эмиссия начнется с самого начала.
+ */
+fun connectableObservableExample3() {
+    val observable2 = Observable.interval(1, TimeUnit.SECONDS)
+        .doOnDispose { println("Observable dispose") }
+        .doOnComplete { println("Observable complete") }
+        .share()
+
+    val sub1 = observable2.subscribe({
+        println("sub 1 received $it")
+        Thread.sleep(500)
+    }, {
+        println("sub 1 error")
+    }, {
+        println("sub 1 complete")
+    })
+
+    Thread.sleep(3000)
+
+    val sub2 = observable2.subscribe({
+        println("sub 2 received $it")
+        Thread.sleep(500)
+    }, {
+        println("sub 2 error")
+    }, {
+        println("sub 2 complete")
+    })
+
+    sub1.dispose()
+
+    Thread.sleep(3000)
+
+    sub2.dispose()
+
+    Thread.sleep(10000)
+}
+
+/**Данный метод возвращает Observable, который будет жить даже после вызова dispose() на всех подписках
+ * Если снова создать новую подписку то эмиссия элементов продолжится с последнего успешно-отправленного!
+ */
+fun connectableObservableExample4() {
+
+    val observable = Observable.interval(1, TimeUnit.SECONDS)
+        .doOnDispose { println("Observable dispose") }
+        .doOnComplete { println("Observable complete") }
+        .publish().autoConnect()
+
+    val sub1 = observable.subscribe({
+        println("sub 1 received $it")
+        Thread.sleep(500)
+    }, {
+        println("sub 1 error")
+    }, {
+        println("sub 1 complete")
+    })
+
+    Thread.sleep(3000)
+
+
+    sub1.dispose()
+
+    val sub2 = observable.subscribe({
+        println("sub 2 received $it")
+        Thread.sleep(500)
+
+    }, {
+        println("sub 2 error")
+    }, {
+        println("sub 2 complete")
+    })
+
+    Thread.sleep(3000)
+
+    sub2.dispose()
+
+    val sub3 = observable.subscribe({
+        println("sub 3 received $it")
+        Thread.sleep(500)
+    }, {
+        println("sub 3 error")
+    }, {
+        println("sub 3 complete")
+    })
+
+    Thread.sleep(3000)
+
+    sub3.dispose()
+
+    Thread.sleep(20000)
+
+}
+
+// TODO: replay().refcount() разобрать отдельно
+fun connectableObservableExample5() {
+    val observable = Observable.interval(1, TimeUnit.MILLISECONDS)
+        .doOnDispose { println("Observable dispose") }
+        .doOnComplete { println("Observable complete") }
+        .replay(3)
+        .refCount()
+
+    val sub1 = observable.subscribe({
+        println("sub 1 received $it")
+        Thread.sleep(250)
+    }, {
+        println("sub 1 error")
+    }, {
+        println("sub 1 complete")
+    })
+
+    Thread.sleep(3000)
+
+    //Если отписка произошла, то новый подписчик будет принимать значения с самого начала
+//    sub1.dispose()
+
+    val sub2 = observable.subscribe({
+        println("sub 2 received $it")
+        Thread.sleep(250)
+    }, {
+        println("sub 2 error")
+    }, {
+        println("sub 2 complete")
+    })
 
     Thread.sleep(10000)
 }
