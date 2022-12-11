c6714c76cfe38c19f530458a8ecb8211e2ad4d92
Igor Novikov <igor.novikov@umbrellait.com>
2022-12-11 15:51:09 +0300

    Error handling

---
+++ src/main/kotlin/8.ErrorHandling.kt
@@ -0,0 +1,77 @@
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+import java.lang.RuntimeException
+
+fun main() {
+//    errorHandlingExample1()
+//    errorHandlingExample2()
+//    errorHandlingExample3()
+//    errorHandlingExample4()
+    errorHandlingExample5()
+}
+
+/** Если не указать обработчик ошибок, то при получении ошибки в цепи -
+ * приложение свалится по io.reactivex.exceptions.OnErrorNotImplementedException: Error*/
+
+fun errorHandlingExample1() {
+    Observable.create<String> { emitter ->
+        emitter.onNext("Hello")
+        emitter.onNext("World")
+        emitter.onError(RuntimeException("Error"))
+        emitter.onComplete()
+    }
+        .subscribe {
+            println(it)
+        }
+}
+
+/** Вернет предопределенное значение если произойдет ошибка. Обрати внимание - оператор принимает Function.*/
+fun errorHandlingExample2() {
+    Observable.create<String> { emitter ->
+        emitter.onNext("Hello")
+        emitter.onNext("World")
+        emitter.onError(RuntimeException("Error"))
+        emitter.onComplete()
+    }.onErrorReturn { "Error happens" }
+        .subscribe {
+            println(it)
+        }
+}
+
+/*При возникновении ошибки - произойдет переподписка на другой Observable*/
+fun errorHandlingExample3() {
+    Observable.create<String> { emitter ->
+        emitter.onNext("Hello")
+        emitter.onNext("World")
+        emitter.onError(RuntimeException("Error"))
+        emitter.onComplete()
+    }.onErrorResumeNext(Observable.just("String from new Observable"))
+        .subscribe {
+            println(it)
+        }
+}
+
+/** Будет переподпиcываться несколько раз всякий раз когда будет приходить ошибка
+ * Если 3 раза эта ошибка повторится - прокинет ее дальше по цепочки
+ * */
+fun errorHandlingExample4() {
+    Observable.just(1, 2, 3, 4, 5)
+        .map { it / (3 - it) }
+        .retry(3)
+        .subscribeBy(
+            onNext = { println("Received $it") },
+            onError = { println("Error") })
+}
+
+/** Переподписка будет происходить всякий раз пока условие удовлетворяет предикату*/
+fun errorHandlingExample5() {
+    var retryCount = 0
+    Observable.just(1, 2, 3, 4, 5)
+        .map { it / (3 - it) }
+        .retry { _, _ ->
+            (++retryCount) < 3
+        }
+        .subscribeBy(
+            onNext = { println("Received $it") }, onError = { println("Error") }
+        )
+}
