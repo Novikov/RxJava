5cedcb557ebc8f2e28e4e254d398776342c2b5af
Igor Novikov <igor.novikov@umbrellait.com>
2023-01-07 21:32:50 +0300

    tasks

--- src/main/kotlin/10.SubsribeObserveOn.kt
+++ src/main/kotlin/10.SubsribeObserveOn.kt
@@ -1,22 +1,23 @@
 import io.reactivex.Observable
+import io.reactivex.ObservableOperator
 import io.reactivex.Observer
-import io.reactivex.Scheduler
 import io.reactivex.disposables.Disposable
-import io.reactivex.internal.schedulers.ComputationScheduler
 import io.reactivex.schedulers.Schedulers
 
 fun main() {
-//    threadingExample1()
-//    threadingExample2()
-    threadingExample3()
+//    withoutChangingThreadExample()
+//    subscribeOnExample()
+//    observeOnExample()
+//    unionSubscribeOnObserveOn()
+//    doubleObserveOnExample()
+//    doubleSubscribeOnExample()
+    doubleSubscribeOnExample2()
 }
 
 /** Вся теория рассказана тут https://habr.com/ru/company/rambler_and_co/blog/280388 */
 
-/** Cold Observable example - Выпуск значений произойдет только после подписки */
-
 /** Без вызова методов observeOn/subscribeOn все элементы эмиссии будут создаваться и потребляться на главном потоке */
-fun threadingExample1() {
+fun withoutChangingThreadExample() {
     val observable: Observable<String> = Observable.create<String> {
         println("Inside start observable [thread] - ${Thread.currentThread().name}")
         it.onNext("Emission Emit 1")
@@ -49,11 +50,11 @@ fun threadingExample1() {
     Thread.sleep(3000)
 }
 
-/** subscribeOn() -  с помощью этого оператора можно указать Scheduler, в котором будет выполняться работа Observable
+/** subscribeOn() - поток, в котором будет выполняться работа Observable
  *
- * При использовании без observeOn() - влияет как на операции эмиссии так и на операции потребления. На main выполнится только onSubscribe()
+ * При использовании без observeOn() - влияет как на поток эмиссии так и на поток потребления. На main выполнится только onSubscribe()
  */
-fun threadingExample2() {
+fun subscribeOnExample() {
     val observable: Observable<String> = Observable.create<String> {
         println("Inside start observable [thread] - ${Thread.currentThread().name}")
         it.onNext("Emission Emit 1")
@@ -88,11 +89,9 @@ fun threadingExample2() {
     Thread.sleep(3000)
 }
 
-/** observeOn() -  что применение этого оператора приводит к тому, что последующие операции над “излученными” данными
- * будут выполняться с помощью Scheduler, переданным в этот метод
- *
- * При использовании без subscribeOn() - влияет только на операции потребления данных. Выпуск и onSubscribe будет на main scheduler*/
-fun threadingExample3() {
+/** observeOn() - поток, в котором будет выполняться эмиссия данных из Observable
+ * При использовании без subscribeOn() - влияет только на поток потребления данных. Выпуск и onSubscribe будет на main scheduler*/
+fun observeOnExample() {
     val observable: Observable<String> = Observable.create<String> {
         println("Inside start observable [thread] - ${Thread.currentThread().name}")
         it.onNext("Emission Emit 1")
@@ -122,6 +121,158 @@ fun threadingExample3() {
     }
     observable
         .observeOn(Schedulers.io())
+        .subscribe(observer)
+
+    Thread.sleep(3000)
+}
+
+/** В результате объединения subscribeOn + observeOn - эмиссия будет происходить на Computation, а поглощение на IO*/
+fun unionSubscribeOnObserveOn() {
+    val observable: Observable<String> = Observable.create<String> {
+        println("Inside start observable [thread] - ${Thread.currentThread().name}")
+        it.onNext("Emission Emit 1")
+        it.onNext("Emit 2")
+        println("Inside intermediate observable [thread] - ${Thread.currentThread().name}")
+        it.onNext("Emit 3")
+        it.onNext("Emit 4")
+        it.onComplete()
+    }
+
+    val observer: Observer<String> = object : Observer<String> {
+        override fun onComplete() {
+            println("Consumption all Completed [thread] - ${Thread.currentThread().name}\"")
+        }
+
+        override fun onNext(item: String) {
+            println("Consumption next $item [thread] - ${Thread.currentThread().name}")
+        }
+
+        override fun onError(e: Throwable) {
+            println("Consumption error Occured ${e.message} [thread] - ${Thread.currentThread().name}\"")
+        }
+
+        override fun onSubscribe(d: Disposable) {
+            println("onSubscribe [thread] - ${Thread.currentThread().name}\"")
+        }
+    }
+    observable
+        .subscribeOn(Schedulers.computation())
+        .observeOn(Schedulers.io())
+        .subscribe(observer)
+
+    Thread.sleep(3000)
+}
+
+/** Второй observeOn изменит поток потребления который был установлен до этого по цепочке выше*/
+fun doubleObserveOnExample() {
+    val observable: Observable<String> = Observable.create<String> {
+        println("Inside start observable [thread] - ${Thread.currentThread().name}")
+        it.onNext("Emission Emit 1")
+        it.onNext("Emit 2")
+        println("Inside intermediate observable [thread] - ${Thread.currentThread().name}")
+        it.onNext("Emit 3")
+        it.onNext("Emit 4")
+        it.onComplete()
+    }
+
+    val observer: Observer<String> = object : Observer<String> {
+        override fun onComplete() {
+            println("Consumption all Completed [thread] - ${Thread.currentThread().name}\"")
+        }
+
+        override fun onNext(item: String) {
+            println("Consumption next $item [thread] - ${Thread.currentThread().name}")
+        }
+
+        override fun onError(e: Throwable) {
+            println("Consumption error Occured ${e.message} [thread] - ${Thread.currentThread().name}\"")
+        }
+
+        override fun onSubscribe(d: Disposable) {
+            println("onSubscribe [thread] - ${Thread.currentThread().name}\"")
+        }
+    }
+    observable
+        .observeOn(Schedulers.io())
+        .observeOn(Schedulers.computation())
+        .subscribe(observer)
+
+    Thread.sleep(3000)
+}
+
+/** Второй subscribeOn никак не повлияет на поток эмиссии и потребления данных*/
+fun doubleSubscribeOnExample() {
+    val observable: Observable<String> = Observable.create<String> {
+        println("Inside start observable [thread] - ${Thread.currentThread().name}")
+        it.onNext("Emission Emit 1")
+        it.onNext("Emit 2")
+        println("Inside intermediate observable [thread] - ${Thread.currentThread().name}")
+        it.onNext("Emit 3")
+        it.onNext("Emit 4")
+        it.onComplete()
+    }
+
+    val observer: Observer<String> = object : Observer<String> {
+        override fun onComplete() {
+            println("Consumption all Completed [thread] - ${Thread.currentThread().name}\"")
+        }
+
+        override fun onNext(item: String) {
+            println("Consumption next $item [thread] - ${Thread.currentThread().name}")
+        }
+
+        override fun onError(e: Throwable) {
+            println("Consumption error Occured ${e.message} [thread] - ${Thread.currentThread().name}\"")
+        }
+
+        override fun onSubscribe(d: Disposable) {
+            println("onSubscribe [thread] - ${Thread.currentThread().name}\"")
+        }
+    }
+    observable
+        .subscribeOn(Schedulers.io())
+        .subscribeOn(Schedulers.computation())
+        .subscribe(observer)
+
+    Thread.sleep(3000)
+}
+
+/** с lyft можно добиться желаемого поведения как с doubleObserveOn()*/
+fun doubleSubscribeOnExample2() {
+    val observable: Observable<String> = Observable.create<String> {
+        println("Inside start observable [thread] - ${Thread.currentThread().name}")
+        it.onNext("Emission Emit 1")
+        it.onNext("Emit 2")
+        println("Inside intermediate observable [thread] - ${Thread.currentThread().name}")
+        it.onNext("Emit 3")
+        it.onNext("Emit 4")
+        it.onComplete()
+    }
+
+    val observer: Observer<String> = object : Observer<String> {
+        override fun onComplete() {
+            println("Consumption all Completed [thread] - ${Thread.currentThread().name}\"")
+        }
+
+        override fun onNext(item: String) {
+            println("Consumption next $item [thread] - ${Thread.currentThread().name}")
+        }
+
+        override fun onError(e: Throwable) {
+            println("Consumption error Occured ${e.message} [thread] - ${Thread.currentThread().name}\"")
+        }
+
+        override fun onSubscribe(d: Disposable) {
+            println("onSubscribe [thread] - ${Thread.currentThread().name}\"")
+        }
+    }
+    observable
+        .subscribeOn(Schedulers.io())
+        .lift(ObservableOperator {
+            println("inside lift [thread] - ${Thread.currentThread().name}\"")  /** Тут можно получить поток из Scheduler указанного после lift. Насколько я понял тут с помощью list можно трансфортировать Observable. Для этого нужно разобрать принцип работы оператора*/
+            it
+        })
+        .subscribeOn(Schedulers.computation())
         .subscribe(observer)
 
     Thread.sleep(3000)
