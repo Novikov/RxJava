59a49d9d404f84f16e5f840159fcecbaf621a78b
Igor Novikov <igor.novikov@umbrellait.com>
2023-01-10 10:01:13 +0300

    testing

--- src/test/kotlin/RxTestExample1.kt
+++ src/test/kotlin/RxTestExample1.kt
@@ -1,2 +1,16 @@
+import io.reactivex.Observable
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+
 class RxTestExample1 {
+
+    @Test
+    fun testBlockingSubscribe(){
+        val hitcount = AtomicInteger()
+        val source = Observable.interval(1, TimeUnit.SECONDS).take(5)
+        source.blockingSubscribe { hitcount.incrementAndGet() }
+        assertTrue(hitcount.get() == 5)
+    }
 }
